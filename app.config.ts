10baf4c698790cb439650a3c1edfe0123c28f6cf
ashfaq patwari <ashfaqpatwari@gmail.com>
2025-08-09 17:56:33 +0530

    try preview channel

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Shift Gift Me",
   slug: "registry-app",
-  version: "1.0.0",
+  version: "1.0.2",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "shiftgiftme",
